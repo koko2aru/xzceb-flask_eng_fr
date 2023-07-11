844542ba4bd62e810367c2109d0632b1eb36651b
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-11 19:42:06 +0200

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,9 +1,14 @@
-import os
-import json
+#Translator
 from deep_translator import MyMemoryTranslator
 
-
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source='en', target='fr').translate(englishText)
-    print(frenchText)
-    return frenchText
+def english_to_french(english_text):
+    #Translator English to French
+    french_text = MyMemoryTranslator(source='GB-en', target='FR-fr').translate(english_text)
+    print(french_text)
+    return french_text
+    
+def french_to_english(french_text):
+    #Translator French to English
+    english_text = MyMemoryTranslator(source='FT-fr', target='GB-en').translate(french_text)
+    print(english_text)
+    return english_text
