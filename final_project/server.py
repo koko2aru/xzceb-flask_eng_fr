9cea486b590309699b24484c89362e7d5b8af721
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-12 11:24:42 +0200

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -13,12 +13,13 @@ def englishToFrench():
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translated_text = french_to_english(textToTranslate)
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    # render index.html
+    render("static/index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
