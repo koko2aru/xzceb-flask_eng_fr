5ba2d8696c342d56e8f2a1c2bfaac16dc15b8e79
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-13 15:29:03 +0200

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,25 +1,27 @@
-from machinetranslation import translator
+import machinetranslation as machine
 from flask import Flask, render_template, request
 import json
+from deep_translator import MyMemoryTranslator
+
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    translated_text = english_to_french(textToTranslate)
-    return translated_text
+    translated_text = machine.translator.english_to_french(textToTranslate)
+    return render_template("index.html", translated_text=translated_text)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    translated_text = french_to_english(textToTranslate)
-    return translated_text
+    translated_text = machine.translator.french_to_english(textToTranslate)
+    return render_template("index.html", translated_text=translated_text)
 
 @app.route("/")
 def renderIndexPage():
     # render index.html
-    render("static/index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
