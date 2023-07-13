c0d619b75952d6846f03fce4f1fae98390724172
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-13 15:31:40 +0200

    Create test.py

---
+++ final_project/machinetranslation/test.py
@@ -0,0 +1,15 @@
+#unit test.py
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestEnglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertNotEqual(english_to_french("Bad"), "Bonjour")  
+
+class TestFrenchToEnglish(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(french_to_english("Bonjour"), "good") 
+        
+unittest.main()
